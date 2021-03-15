88d791b6e5904de9d274765da4770f47c178293c
trantvivian <trantvivian@gmail.com>
2021-03-14 20:34:36 -0400

    changed themes from local storage to session storage

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link rel="stylesheet" href="./themes/color.css">
+        <link rel="stylesheet" href="./themes/purple.css">
         <link rel="stylesheet" id="switcher-id" href="">
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -116,7 +116,7 @@
                 </div>
                 <script>
                   let switches = document.getElementsByClassName('switch');
-                  let style = localStorage.getItem('style');
+                  let style = sessionStorage.getItem('style');
                   if (style == null) {
                     setTheme('light');
                   } else {
@@ -140,7 +140,7 @@
                     } else if (theme == 'dark') {
                       document.getElementById('switcher-id').href = './themes/dark.css';
                     }
-                    localStorage.setItem('style', theme);
+                    sessionStorage.setItem('style', theme);
                   }
                   </script>
                 <div class="form-group">
--- queue.php
+++ queue.php
@@ -10,7 +10,7 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-        <link rel="stylesheet" href="styles/style.css">
+        <link rel="stylesheet" href="./themes/purple.css">
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1"> 
@@ -70,7 +70,12 @@
               </div>
             </div>
           </div>
-
+          
           <script type="text/javascript" src="js/script.js"></script>
+          <script>
+            window.onload = function() {
+              sessionStorage.getItem('style');
+            }
+          </script>
     </body>
 </html>
