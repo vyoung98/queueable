d0f06fd417a36e7e033971d26b94094a66cce44e
vyoung98 <val.young98@gmail.com>
2021-03-15 22:08:48 -0400

    stuff

--- index.php
+++ index.php
@@ -30,6 +30,22 @@
     </head>
     <body>
       <?php include('header.html')?>
+      <?php
+      switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
+        case '/':                   // URL (without file name) to a default screen
+            require 'index.php';
+            break; 
+        case '/queue.php':     // if you plan to also allow a URL with the file name 
+            require 'queue.php';
+            break;              
+        case '/eventcreation.php':
+            require 'eventcreation.php';
+            break;
+        default:
+            http_response_code(404);
+            exit('Not Found');
+      }  
+      ?>
 
           <div class="container">
             <div class="row">
