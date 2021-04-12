30e915fbe03bf57f76ab5c2b8a12d1c8c41fbead
vyoung98 <val.young98@gmail.com>
2021-04-12 18:31:43 -0400

    some gcp things

--- index.php
+++ index.php
@@ -9,6 +9,31 @@
         case '/eventcreation.php':
             require 'eventcreation.php';
             break;
+        case '/settings.php':
+            require 'settings.php';
+            break;
+        case '/login.php':
+            require 'login.php';
+            break;        
+        case '/signup.php':
+            require 'signup.php';
+            break;
+        case '/submit.php':
+            require 'submit.php';
+            break;
+        case '/signin.php':
+            require 'signin.php';
+            break;
+        case '/changee.php':
+            require 'changee.php';
+            break;
+        case '/changep.php':
+            require 'changep.php';
+            break;
+        case '/connect-db.php':
+            require 'connect-db.php';
+            break;
+
         default:
             http_response_code(404);
             exit('Not Found');
