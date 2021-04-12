8597d88077dc483eb8e9c35d7f50d9c0a43d4eaa
vyoung98 <val.young98@gmail.com>
2021-04-12 17:10:34 -0400

    change password works

--- change.php
+++ change.php
@@ -6,10 +6,14 @@ $password = $_POST['newPassword'];
 $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 $username = $_SESSION['user'];
 echo $username;
-$query = "UPDATE users SET password='$hashed_password' WHERE username='$username'";
+echo $hashed_password;
+$query = "UPDATE users SET hashed_password=:hashed_password WHERE username=:username";
 $statement = $db->prepare($query);
-$statement->bindValue('hashed_password', $hashed_password);
+$statement->bindValue(':hashed_password', $hashed_password);
+$statement->bindValue(':username', $username);
 $statement->execute();
 $statement->closeCursor();
+
+header("Location: login.php");
 
 ?>
