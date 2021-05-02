4078fc10e99467238ad4e138dde1e71dffcdf4c1
trantvivian <trantvivian@gmail.com>
2021-05-01 23:11:40 -0400

    themes notw orking

--- settings.php
+++ settings.php
@@ -13,10 +13,11 @@
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (!empty($_POST['action']) && ($_POST['action'] == 'Favorite')) {
       $username = $_SESSION['user'];
-      $theme = $_POST['pName2'];
+      $theme = $_POST['switcher-id'];
       $query = "UPDATE settings SET theme=:theme WHERE username=:username";
       $statement = $db->prepare($query);
       $statement->bindValue(':theme', $theme);
+      $statement->bindValue(':username', $username);
       $statement->execute();
       $statement->closeCursor();
       echo "Theme changed and saved";
@@ -144,10 +145,12 @@
               <div class="col">
                 <h2>Theme Selector</h2>
                 <div class="theme-switches">
-                <div data-theme="light" class="switch" id="switch-1" method="POST"></div>
-                <div data-theme="sky" class="switch" id="switch-2" method="POST"></div>
-                <div data-theme="purple" class="switch" id="switch-3" method="POST"></div>
-                <div data-theme="dark" class="switch" id="switch-4" method="POST"></div>
+                <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+                <div data-theme="light" class="switch" id="switch-1" method="POST" name="action"></div>
+                <div data-theme="sky" class="switch" id="switch-2" method="POST" name="action"></div>
+                <div data-theme="purple" class="switch" id="switch-3" method="POST" name="action"></div>
+                <div data-theme="dark" class="switch" id="switch-4" method="POST" name="action"></div>
+                </form>
                 </div>
                 <script>
                   let switches = document.getElementsByClassName('switch');
@@ -158,15 +161,6 @@
                       console.log(savedtheme);
                       setTheme(theme);
                       // THIS ARROW FUNCTION SHOWS THE THEME WHEN THE BUTTON IS CLICKED
-                      
-                      <?php 
-                      $query = "UPDATE settings SET theme=:theme WHERE username=:username";
-                      $statement = $db->prepare($query);
-                      $statement->bindValue(':theme', $theme);
-                      $statement->bindValue(':username', $username);
-                      $statement->execute();
-                      $statement->closeCursor();
-                      ?>
                       let color = () => {
                         return theme;
                       } 
