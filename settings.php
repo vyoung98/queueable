0f9d10682215e49c5ca04c7e8d348416a2bd6cf6
trantvivian <trantvivian@gmail.com>
2021-05-03 18:29:57 -0400

    about page updated

--- settings.php
+++ settings.php
@@ -23,12 +23,13 @@
         <link rel="icon" type="image/png" sizes="32x32" href="icon/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="icon/favicon-16x16.png">
         <link rel="manifest" href="icon/site.webmanifest">
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+<!-- FONT -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
+        <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-        <link rel="stylesheet" href="./themes/purple.css">
+        <link rel="stylesheet" href="./themes/purple.css" type="text/css" />
         <link rel="stylesheet" id="switcher-id" href="">
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -37,15 +38,6 @@
         <meta name="author" content="Vivian Tran vt5en & Valerie Young vy5br">
         <meta name="description" content="POTD 1">  
         <title>queueAble: Finding Time for What's Important</title>  
-        <style type="text/css">
-            h1	{font-family: 'Rubik', Arial;
-                }
-            h2   {font-family: 'Rubik', 'Arial';
-                  font-size: 25px;
-                }
-            p   {font-family: 'Rubik', 'Arial';
-                }
-        </style>
        
     </head>
     <?php include('navbar.php')?>
