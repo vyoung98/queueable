59eb22c4f49cac57cb921b845e3d0e33987b70c4
vyoung98 <val.young98@gmail.com>
2021-05-03 04:55:47 -0400

    email finally works

--- queueable/src/app/friend-form/friend-form.component.ts
+++ queueable/src/app/friend-form/friend-form.component.ts
@@ -11,7 +11,7 @@ import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
 })
 export class FriendFormComponent {
 
-  model = new Friend('Viv', 'Val', 'vt5en@virginia.edu', 'vy5br@virginia.edu', 'I want to play Stardew Valley with you!');
+  model = new Friend('Viv', 'Val', 'viv@gmail.com', 'val@gmail.com', 'I want to play Stardew Valley with you!');
 
   submitted = false;
 
@@ -21,10 +21,14 @@ export class FriendFormComponent {
     this.model = new Friend('', '', '', '', '');
   }
 
+  // the following lines break the code
+  // constructor(private http: HttpClient){ 
+  //   this.sendPost(this.newFriend);
+  // }
 
-
-  sendPost(data: any): Observable<any>{ 
-    //send post to PHP
-    return this.http.post('http://localhost/CS-4640-Project/PHP/ngphp-post.php', data);
-  }
+  // sendPost(data: any): Observable<any>{ 
+  //   //send post to PHP
+  //   // return "hello";
+  //   return this.http.post('http://localhost/CS-4640-Project/PHP/ngphp-post.php', data);
+  // }
 }
--- queueable/src/app/friend.ts
+++ queueable/src/app/friend.ts
@@ -1,12 +1,15 @@
+// import {Http} from '@angular/http';
+// import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+
 export class Friend {
 
     constructor(
+      // private http: HttpClient,
       public name: string,
       public f_name: string,
       public email: string,
       public f_email: string,
-      public message: string
-    ) {  }
+      public message: string,
+    ) { }
   
   }
-  
