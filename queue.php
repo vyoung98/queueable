1583383d68f8b53bae3f189190c1ad6ee7d64bd8
trantvivian <trantvivian@gmail.com>
2021-05-02 23:01:58 -0400

    styles

--- queue.php
+++ queue.php
@@ -17,7 +17,7 @@
         <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
         <link rel="stylesheet" href="./themes/purple.css">
-        <link rel="stylesheet" href="./styles/style.css">
+        <!-- <link rel="stylesheet" href="./styles/style.css"> -->
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1"> 
