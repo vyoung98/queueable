bfbe1a4dadfbbffb22930813eca3b8805168468b
trantvivian <trantvivian@gmail.com>
2021-05-03 17:23:12 -0400

    message

--- ng-post.php
+++ ng-post.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: http://localhost:4200');
 
 header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding');
 
@@ -14,6 +14,7 @@ $content_length = (int) $_SERVER['CONTENT_LENGTH'];
 //retrieve data
 $postdata = file_get_contents("php://input");
 
+//echo $postdata;
 //extract
 $request = json_decode($postdata);
 
@@ -21,24 +22,29 @@ $data = [];
 $data[0]['length'] = $content_length;
 foreach ($request as $k => $v) {
     $temp = "$k => $v";
+    //echo $temp;
     $data[0]['gcp_post_'.$k] = $v;
 }
-
 //base function
-function addFriend($name, $email, $f_name, $f_email) {
+function addFriend($name, $email, $f_name, $f_email, $message) {
     require('connect-db.php');
-    $query = "INSERT INTO friends (name, email, f_name, f_email) VALUES (:name, :email, :f_name, :f_email)";
-
+    $query = "INSERT INTO friends (name, email, f_name, f_email, message) VALUES (:name, :email, :f_name, :f_email, :message)";
     $statement = $db -> prepare($query);
     $statement->bindValue(':name', $name);
     $statement->bindValue(':email', $email);
     $statement->bindValue(':f_name', $f_name);
     $statement->bindValue(':f_email', $f_email);
+    $statement->bindValue(':message', $message);
     $statement->execute();
     $statement->closeCursor();
 }
-if(isset($request->email)) {
-    addFriend($request->name, $request-> email, $request->f_name, $request->f_email);
+// echo $request->name;
+// echo $request->email;
+// echo $request->f_name;
+// echo $request->f_email;
+$current_date = date("Y-m-d");
+if(isset($request->name)) {
+    addFriend($request->name, $request-> email, $request->f_name, $request->f_email, $request->message);
 }
-echo json_encode(['content'=>$data, 'response_on'=> $current_date]);
+//echo json_encode(['content'=>$data, 'response_on'=> $current_date]);
 ?>
