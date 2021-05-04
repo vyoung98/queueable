c16f5e0c38fb25bb4236af2ad3c539881f7fb3c8
vyoung98 <val.young98@gmail.com>
2021-05-03 22:16:22 -0400

    footer

--- footer.php
+++ footer.php
@@ -1,14 +1,15 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<footer class="navbar navbar-expand-lg navbar-light bg-light align-bottom">
+    <footer class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
-
         <li class="nav-item align-right">
           <a class="nav-link" href="https://www.linkedin.com/in/vivian-t-972418156/">Vivian Tran (vt5en)</a>
         </li>
           <li class="nav-item align-right">
             <a class="nav-link" href="https://www.linkedin.com/in/valerie-young98/">Valerie Young (vy5br)</a>
           </li>
+          <li><small>
+            © Val and Viv
+            </small></li>
       </ul>
-    </div>
-  </nav>
+    </footer>
+</footer>
