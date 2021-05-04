9fb417aae764068bdf51753fcf962fa4cf280204
trantvivian <trantvivian@gmail.com>
2021-05-03 20:27:06 -0400

    things

--- about.php
+++ about.php
@@ -10,7 +10,7 @@
 
 ?>
 <head>
-    <link rel="apple-touch-icon" sizes="180x180" href="icon/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="180x180" href="icon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="icon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="icon/favicon-16x16.png">
     <link rel="manifest" href="icon/site.webmanifest">
